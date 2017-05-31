39c882565a7bc5988e225fdd8f2d6b5f8953bd8b
Jared Murphy <jaredcmurphy@gmail.com>
2017-05-31 12:02:14 -0400

    messaging working between users without roles

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,31 @@
+class ConversationsController < ApplicationController
+  before_action :logged_in
+
+  def index
+   @users = User.all
+   @conversations = Conversation.all
+  end
+
+  def create
+   if Conversation.between(params[:sender_id],params[:recipient_id])
+     .present?
+      @conversation = Conversation.between(params[:sender_id],
+       params[:recipient_id]).first
+   else
+     @conversation = Conversation.create!(conversation_params)
+   end
+   redirect_to conversation_messages_path(@conversation)
+  end
+
+  private
+
+  def logged_in
+    if !current_user
+      redirect_to "/users/sign_in"
+    end
+  end
+
+   def conversation_params
+    params.permit(:sender_id, :recipient_id)
+   end
+end
